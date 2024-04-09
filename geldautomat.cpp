69bb8dd7af193802043101acb87a0079cfa16a91
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2024-04-09 14:10:09 +0800

    Erster Commit mit dem Menübildschirm

---
+++ geldautomat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void zeigeDasMenu() {
+    cout << "******Menu******" << endl;
+    cout << "1. Check Balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "****************" << endl;
+}
+
+int main() {
+    // check balance, deposit, withdraw, menu
+    zeigeDasMenu();
+    system("pause > 0");
+}
